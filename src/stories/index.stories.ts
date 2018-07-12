09264594f5faa31e39b7242fd931305e949b5e2a
Naresh Bhatia <naresh.archfirst@gmail.com>
2018-07-12 15:35:06 -0400

    Used moduleMetadata() decorator to factor out common metadata

--- src/stories/index.stories.ts
+++ src/stories/index.stories.ts
@@ -1,4 +1,4 @@
-import { storiesOf } from '@storybook/angular';
+import { storiesOf, moduleMetadata } from '@storybook/angular';
 import { withNotes } from '@storybook/addon-notes';
 import { action } from '@storybook/addon-actions';
 import { linkTo } from '@storybook/addon-links';
@@ -8,20 +8,19 @@ import { OrderViewComponent } from '../app/order-view/order-view.component';
 import { OrderProgressBarComponent } from '../app/order-progress-bar/order-progress-bar.component';
 
 storiesOf('Order View', module)
+    .addDecorator(
+        moduleMetadata({
+            declarations: [OrderProgressBarComponent]
+        })
+    )
     .add('buy order', () => ({
         component: OrderViewComponent,
-        moduleMetadata: {
-            declarations: [OrderProgressBarComponent]
-        },
         props: {
             order: new Order('o100', Side.BUY, 'GOOG', 10000, 6000, 4000)
         }
     }))
     .add('sell order', () => ({
         component: OrderViewComponent,
-        moduleMetadata: {
-            declarations: [OrderProgressBarComponent]
-        },
         props: {
             order: new Order('o200', Side.SELL, 'HAL', 5000, 4500, 4000)
         }
