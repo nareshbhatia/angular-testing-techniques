6e820622d34e302fb52a3dcfb1035edf87b8a78f
Naresh Bhatia <naresh.archfirst@gmail.com>
2018-09-17 20:38:02 -0400

    Relaxed image matching requirements to compensate for different environments

--- snapshot-testing/home-page.test.js
+++ snapshot-testing/home-page.test.js
@@ -4,16 +4,31 @@ describe('Home page', () => {
     let browser;
 
     beforeAll(async () => {
-        browser = await puppeteer.launch();
+        browser = await puppeteer.launch({ headless: false });
     });
 
     it('works', async () => {
+        // Create a fresh browser page
         const page = await browser.newPage();
-        await page.setViewport({width: 460, height: 220, deviceScaleFactor: 1});
+
+        // Set the desired viewport size
+        await page.setViewport({
+            width: 460,
+            height: 220,
+            deviceScaleFactor: 1
+        });
+
+        // Navigate to home page
         await page.goto('http://localhost:4200/');
+
+        // Take a screen shot
         const image = await page.screenshot();
 
-        expect(image).toMatchImageSnapshot();
+        // Allow 2% difference in images to compensate for different environments
+        expect(image).toMatchImageSnapshot({
+            failureThresholdType: 'percent',
+            failureThreshold: 0.02
+        });
     });
 
     afterAll(async () => {
